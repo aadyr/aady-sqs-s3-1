5787457c6729fe5aff7f2e756e2f169f9d839f18
aadyr <aadharsh11@gmail.com>
2023-07-25 21:37:01 +0800

    s3-sqs-changes

---
+++ main.tf
@@ -0,0 +1,38 @@
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = ["arn:aws:sqs:*:*:aady-s3-event-notification-queue"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue" "queue" {
+  name   = "aady-s3-event-notification-queue"
+  policy = data.aws_iam_policy_document.queue.json
+}
+
+resource "aws_s3_bucket" "bucket" {
+bucket = "aady-sctp-ce2-tfstate-bkt"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+    
+  }
+}
